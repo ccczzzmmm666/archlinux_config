b44dd12e14f91f731d8601e85121d16eaf756732
ccczzzmmm666 <148333286+ccczzzmmm666@users.noreply.github.com>
2024-06-27 17:07:57 +0800

    Add files via upload

--- config.sh
+++ config.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+echo "begin!!!!!!!!!!!!!!!!"
 sudo pacman -S zsh
+echo "finish zshhhhhhhhhhhhhhh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "finish OMZZZZZZZZZZZZZZZZZZZZZZZ"
 sudo pacman -S git
+echo "finish gitttttttttttttttttttttt"
 sudo pacman -S wget
+echo "finish wgettttttttttttttttttttt"
 git clone https://github.com/ccczzzmmm666/archlinux_config.git
+echo "finish cloneeeeeeeeeeeeeeeeeeeeeeeee"
 cat ./archlinux_config/zshrc.sh >~/.zshrc
+echo "finish allllllllllllllllllllllllllll"
